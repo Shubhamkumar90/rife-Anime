e3cb1e8f9051556addde53f608bda131a18930f5
shubham kumar <shubhamkumarshukla48@gmail.com>
2025-06-27 17:03:49 +0530

    resolved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import ImagePicker from '../Components/Imagepicker';
+import ImagePicker from '../Components/ImagePicker';
 import './App.css'
 import axios from 'axios'
 import JSZip from 'jszip'
