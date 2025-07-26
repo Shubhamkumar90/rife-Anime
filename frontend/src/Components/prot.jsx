52df25d71b175bc5c75bfff77e112bdf7084c8b5
shubham kumar <shubhamkumarshukla48@gmail.com>
2025-07-26 21:46:28 +0530

    updating prot

--- frontend/src/Components/prot.jsx
+++ frontend/src/Components/prot.jsx
@@ -2,7 +2,7 @@
 import axios from 'axios';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-export default function Prot({ children,rev }) {
+export default function Prot({ children }) {
     const navigate=useNavigate()
     useEffect(()=>{
         const check=async()=>{
