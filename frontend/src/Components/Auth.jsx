454c9c91881602b6b7ebff7e53c4470c5e9678c7
shubham kumar <shubhamkumarshukla48@gmail.com>
2025-08-03 19:35:45 +0530

    minor update

--- frontend/src/Components/Auth.jsx
+++ frontend/src/Components/Auth.jsx
@@ -32,7 +32,7 @@ function Auth({type}){
                     <form onSubmit={sendcall} className="flex flex-col  justify-center">
                         <label className="text-sm font-medium text-gray-700" htmlFor="email">Email</label>
                         <input type="email" placeholder="Enter email" value={email} onChange={(e)=>setEmail(e.target.value)} className="mt-1 px-4 mb-5 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400" required/>
-                        <label className="text-sm font-medium text-gray-700" htmlFor="password">password</label>
+                        <label className="text-sm font-medium text-gray-700" htmlFor="password">Password</label>
                         <input type="password" placeholder="Password" value={pass} onChange={(e)=>setpass(e.target.value)} className="mt-1 px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400" required/>
                         <div className="text-slate-800 mt-3">
                     {type=="Signin"?"Don't have an account?":"Already have an account?"}
